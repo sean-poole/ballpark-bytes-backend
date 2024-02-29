c69942a7ffd3d0c040762eb6da0940b8923b2426
Sean Poole <spoole5289@gmail.com>
2024-02-28 20:13:49 -0800

    fix file path for env variables

--- config/supabaseConfig.js
+++ config/supabaseConfig.js
@@ -1,6 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, ".env")});
+// require("dotenv").config({ path: path.resolve(__dirname, ".env")});
+require("dotenv").config();
 
 const supabaseURL = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
