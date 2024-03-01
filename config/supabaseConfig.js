effedb7350d388c45cfcbb3d7d9b51a03d505f67
Sean Poole <spoole5289@gmail.com>
2024-02-29 16:02:33 -0800

    remove unused import

--- config/supabaseConfig.js
+++ config/supabaseConfig.js
@@ -1,5 +1,5 @@
 const { createClient } = require("@supabase/supabase-js");
-const path = require("path");
+// const path = require("path");
 // require("dotenv").config({ path: path.resolve(__dirname, ".env")});
 require("dotenv").config();
 
